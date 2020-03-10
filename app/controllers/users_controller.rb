02a4fa5becdb6e61d381b1dd4388285e8c5d18c2
github username <github email address>
2020-03-10 14:39:40 -0400

    add show to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,11 @@ class UsersController < ApplicationController
         @json_user = UserSerializer.new(User.all)
         render json: @json_user
     end
+
+    def show 
+        @user = User.find_by!(id: params[:id])
+        @json_user  = UserSerializer.new(@user)
+        render json: @json_user
+    end
+
 end
